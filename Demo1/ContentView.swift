f5ccfdeeae03e8d405b9adac18ea27aa2dc8e4ed
Jason Wan <njasow@g.ucla.edu>
2024-04-17 03:57:14 -0400

    Initial Commit

---
+++ Demo1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Demo1
+//
+//  Created by jason wan on 2024-04-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo1/Demo1App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo1App.swift
+//  Demo1
+//
+//  Created by jason wan on 2024-04-17.
+//
+
+import SwiftUI
+
+@main
+struct Demo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
